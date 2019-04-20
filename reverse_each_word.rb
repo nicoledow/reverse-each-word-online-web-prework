a2bb5672f2f677717ec8ea6772cd3f9d7f4d2a11
Nicole Dow <nikki.dow71@gmail.com>
2019-04-20 00:27:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,14 @@
-def reverse_each_string(string)
+def reverse_each_word(string)
   new_string = ""
   word_arr = string.split(" ")
-
-  word_arr.collect do |word|
+  
+  word_arr.each_with_index do |word, i|
+  if i <= word_arr.size - 2
+    new_string << word.reverse
+    new_string += " "
+  else
     new_string << word.reverse
   end
-
+  end
   return new_string
 end
-
