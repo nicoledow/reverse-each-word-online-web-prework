eaeae9d62e4d1565f7418ffd245984943152b5fb
Nicole Dow <nikki.dow71@gmail.com>
2019-04-20 00:16:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,8 @@ def reverse_each_word(string)
   word_arr = string.split(" ")
   
   word_arr.each do |word|
-    split_word = word.split("")
-    new_string << split_word.reverse
+    new_string << word.reverse
     new_string += " "
   end
-  
-  return new_string.to_s
+  return new_string
 end
